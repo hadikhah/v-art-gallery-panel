cd0ce72272bbe702b4b36fe8cf9ae92cf518e98d
hadikhah <hadikhahe@gmail.com>
2024-12-28 19:29:13 +0330

    changes redirect route of logged in user

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +22,16 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        RedirectIfAuthenticated::redirectUsing(function ($user) {
+            return route("home", ["lang" => config("app.locale")]);
+        });
+
+
+        ResetPassword::createUrlUsing(function ($user, string $token) {
+            return route("password.reset", ["token" => $token, "lang" => config("app.locale")]) . "?email=" . $user->email;
+        });
+
         Gate::before(function ($user, $ability) {
             return $user->hasRole('admin') ? true : null;
         });
