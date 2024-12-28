c0d76374bcfcf1a3f0ed27450600efea74fe9005
hadikhah <hadikhahe@gmail.com>
2024-12-28 19:57:07 +0330

    adds Icon Components

--- resources/js/Components/Icons/PlusIcon.vue
+++ resources/js/Components/Icons/PlusIcon.vue
@@ -1,16 +1,27 @@
 <script setup>
 const props = defineProps({
     class: {
-        type: Object
-    }
+        type: Object,
+    },
 });
 </script>
 
 <template>
-    <div class="relative -top-3/4 left-1 flex items-center justify-center w-4 h-4 rounded-full border-2 border-gray-900 bg-gray-900 transition-opacity duration-300"
-        :class>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <div
+        class="relative -top-3/4 left-1 flex items-center justify-center w-4 h-4 rounded-full border-2 border-gray-900 bg-gray-900 transition-opacity duration-300 dark:border-gray-600 dark:bg-gray-800"
+        :class="props.class"
+    >
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
             <path d="M12 4v16M4 12h16" />
         </svg>
     </div>
---
+++ resources/js/Components/Icons/ViewGalleryIcon.vue
@@ -0,0 +1,20 @@
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 3h18v18H3V3zm3 3v12h12V6H6zm1 1h10v10H7V7z"
+        />
+    </svg>
+</template>
+
+<script setup>
+// Script can remain empty for simple SVG components
+</script>
