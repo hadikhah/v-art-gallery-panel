d6b78af9ad23236336d1bac7309d1edc3338a53a
hadikhah <hadikhahe@gmail.com>
2024-12-28 19:54:01 +0330

    adds theme toggler button component

---
+++ resources/js/Components/ThemeToggleButton.vue
@@ -0,0 +1,60 @@
+<template>
+    <button
+        type="button"
+        class="relative inline-flex h-6 w-11 items-center rounded-full bg-gray-200 dark:bg-gray-600"
+        @click="toggleDarkMode"
+    >
+        <span
+            :class="[
+                isDark ? 'translate-x-6' : 'translate-x-1',
+                'inline-block h-4 w-4 transform rounded-full transition-transform duration-200 ease-in-out',
+                isDark ? 'bg-gray-700' : 'bg-white shadow-md',
+            ]"
+        >
+            <svg
+                v-if="isDark"
+                xmlns="http://www.w3.org/2000/svg"
+                class="text-yellow-300"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
+            </svg>
+            <svg
+                v-else
+                xmlns="http://www.w3.org/2000/svg"
+                class="text-indigo-500"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+            </svg>
+        </span>
+    </button>
+</template>
+
+<script setup>
+import { useDarkModeStore } from "@/Store/darkMode";
+import { storeToRefs } from "pinia";
+import { onMounted } from "vue";
+
+const darkModeStore = useDarkModeStore();
+const { toggleDarkMode } = darkModeStore;
+const { isDark } = storeToRefs(darkModeStore);
+
+onMounted(() => {
+    darkModeStore.initDarkMode();
+});
+</script>
