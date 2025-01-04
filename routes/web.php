0d35cbcd3ac07eb16be625b6c4ac651ee7ea312f
hadikhah <hadikhahe@gmail.com>
2025-01-04 23:20:51 +0330

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\ExhibitionDefaultSettings;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ExhibitionController;
+use App\Http\Controllers\ExhibitionSettingsController;
 use App\Http\Controllers\ImageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -24,9 +25,7 @@ Route::prefix('{lang?}')->group(function ($route) {
 
         // ------------------ main dashboard page ------------
 
-        Route::get('/dashboard', function () {
-            return Inertia::render('Dashboard');
-        })->name('dashboard');
+        Route::get('/dashboard', [DashboardController::class, "index"])->name('dashboard');
 
         // ------------------ profile ------------
         Route::get('/dashboard/exhibition/list', [ExhibitionController::class, "index"])->name("exhibitions");
@@ -35,7 +34,11 @@ Route::prefix('{lang?}')->group(function ($route) {
 
         Route::get('dashboard/exhibition/create', [ExhibitionController::class, "create"])->name("exhibition.create");
 
-        Route::post('dashboard/exhibition/store', [ExhibitionController::class, "store"])->name("exhibition.store");
+        Route::get('dashboard/exhibition/{exhibition}/settings', [ExhibitionSettingsController::class, "showSettings"])->name("exhibition.settings");
+
+        Route::patch('dashboard/exhibition/{exhibition}/settings', [ExhibitionSettingsController::class, "updateSettings"])->name("exhibition.settings.update");
+
+        Route::post('dashboard/exhibition/{exhibition}/store', [ExhibitionController::class, "store"])->name("exhibition.store");
 
         Route::patch('dashboard/exhibition/{exhibition}/update', [ExhibitionController::class, 'update'])
             ->name("exhibition.update");
@@ -67,9 +70,6 @@ Route::prefix('{lang?}')->group(function ($route) {
 })->middleware(\App\Http\Middleware\Locale::class)
     ->where(["lang" => "fa|en"]);;
 
-
-
-
 Route::get("/exhibition/v30", function () {
     return view("exhibition1");
 });
@@ -85,11 +85,7 @@ Route::get("/exhibition/v100", function () {
 Route::get("/exhibition/visit/{slug}", [ExhibitionController::class, "visit"])->name("exhibition.visit");
 
 Route::get("/exhibition/map", function () {
-    return response()->json([
-        "mapSize" => 1,
-        "cellSize" => 5,
-        "wallThickness" => 0.2
-    ]);
+    return response()->json(ExhibitionDefaultSettings::getDefaultSettings());
 });
 
 Route::get("/exhibition/{exhibition}/images", [ExhibitionController::class, "images"])
