2956ab7264767b323801bd8d32247c9ed78d7c90
hadikhah <hadikhahe@gmail.com>
2025-01-07 07:23:17 +0330

    add type submit as prop to PrimaryButton component

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,5 +1,14 @@
+<script setup>
+defineProps({
+    type: {
+        type: String,
+        default: "submit",
+    },
+});
+</script>
 <template>
     <button
+        :type
         class="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs uppercase tracking-wider text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none active:bg-gray-900 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:active:bg-gray-800"
     >
         <slot />
