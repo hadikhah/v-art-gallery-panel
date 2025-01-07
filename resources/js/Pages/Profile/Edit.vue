c49388979125c21558d7e382d620f92dc83f17ae
hadikhah <hadikhahe@gmail.com>
2025-01-08 00:34:33 +0330

    adds active sessions in profile edit page

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -4,6 +4,7 @@ import DeleteUserForm from "./Partials/DeleteUserForm.vue";
 import UpdatePasswordForm from "./Partials/UpdatePasswordForm.vue";
 import UpdateProfileInformationForm from "./Partials/UpdateProfileInformationForm.vue";
 import { Head } from "@inertiajs/vue3";
+import ActiveSessions from "./Partials/ActiveSessions.vue";
 
 defineProps({
     mustVerifyEmail: {
@@ -11,6 +12,9 @@ defineProps({
     },
     status: {
         type: String,
+    },
+    sessions: {
+        type: Array,
     },
 });
 </script>
@@ -43,6 +47,19 @@ defineProps({
                     class="bg-white dark:bg-gray-800 p-4 shadow sm:rounded-lg sm:p-8"
                 >
                     <UpdatePasswordForm class="max-w-xl" />
+                </div>
+                <!-- Active Sessions -->
+                <div
+                    class="mt-6 overflow-hidden bg-white shadow-xl dark:bg-gray-800 sm:rounded-lg"
+                >
+                    <div class="p-6">
+                        <h3
+                            class="mb-4 text-lg font-semibold text-gray-900 dark:text-gray-100"
+                        >
+                            Active Sessions
+                        </h3>
+                        <ActiveSessions :sessions />
+                    </div>
                 </div>
 
                 <div
