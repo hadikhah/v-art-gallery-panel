1fa72823f4bcdef7b06201e28e928531f44833b5
hadikhah <hadikhahe@gmail.com>
2025-01-08 00:30:19 +0330

    add active sessions and destroySession for removing active session

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\Session;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
@@ -18,10 +20,29 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): Response
     {
+        $sessions = Session::where('user_id', $request->user()->id)
+            ->where('last_activity', '>=', Carbon::now()->subMinutes(config('session.lifetime')))
+            ->get()
+            ->map(callback: fn($session, $index) => Session::mapSession($session, $index));
+
         return Inertia::render('Profile/Edit', [
+            'sessions' => $sessions,
             'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
             'status' => session('status'),
         ]);
+    }
+
+    public function destroySession(Request $request, $id)
+    {
+        if ($id === session()->getId()) {
+            return redirect()->back()->with('error', 'Cannot remove current session');
+        }
+
+        Session::where('id', $id)
+            ->where('user_id', $request->user()->id)
+            ->delete();
+
+        return redirect()->back()->with('success', 'Session removed successfully');
     }
 
     /**
