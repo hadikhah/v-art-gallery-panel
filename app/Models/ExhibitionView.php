ab0989f08a5407f0b5487c28674315c0c31bd3a3
hadikhah <hadikhahe@gmail.com>
2025-01-07 13:55:14 +0330

    add ExhibitionView model

---
+++ app/Models/ExhibitionView.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExhibitionView extends Model
+{
+    protected $fillable = [
+        "exhibition_id",
+        "user_id",
+        "view_date",
+        "view_hour",
+        "views",
+    ];
+
+    protected $casts = [
+        "view_date" => "date",
+    ];
+
+
+    public function exhibition()
+    {
+        return $this->belongsTo(Exhibition::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
